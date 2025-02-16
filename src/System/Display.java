724b88d30737cf7338471b021714c75731ec53a4
Vlad Kovrigin <vladislavkov390@gmail.com>
2025-02-16 03:18:27 +0300

    fix: added validation user input.

--- src/System/Display.java
+++ src/System/Display.java
@@ -6,16 +6,25 @@ import java.util.Scanner;
 
 public class Display {
     public static final String START_MESSAGE = "Начать новую игру (да/нет)?";
+    public static final String ERROR_INPUT_MESSAGE = "ошибка ввода";
     public static String YES = "да";
     public static String NO = "нет";
     public static final Scanner SCANNER = new Scanner(System.in);
 
-    public static void UserStart() {
+    private String userInput;
+
+    public void UserStart() {
         System.out.println(START_MESSAGE);
 
-        if (SCANNER.nextLine().toLowerCase().equals(YES)) {
-            Hangman.HangmanGame();
+        userInput = SCANNER.nextLine();
+
+        while(!(userInput.toLowerCase().equals(YES) || userInput.toLowerCase().equals(NO))){
+            System.out.println(ERROR_INPUT_MESSAGE);
+            userInput = SCANNER.nextLine();
         }
 
+        if (userInput.equals(YES)){
+            Hangman.StartGame();
+        }
     }
 }
