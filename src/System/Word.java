9b81c9b7ecfdf61faa3203e5e32eb70b74fb4e02
Vlad Kovrigin <vladislavkov390@gmail.com>
2025-03-11 15:15:16 +0300

    feat: add check Letter in the word

--- src/System/Word.java
+++ src/System/Word.java
@@ -4,12 +4,13 @@ package System;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 
 public class Word {
-    public static final String PATH_DICTIONARY = "./resource/Dictionary11.txt";
+    public static final String PATH_DICTIONARY = "./resource/Dictionary1.txt";
 
     public static String getRandomWord() {
         try {
@@ -22,5 +23,17 @@ public class Word {
             System.out.println(e.getMessage());
             return null;
         }
+    }
+
+    public static ArrayList<Integer> findLetterPositions(String word, char letter) {
+        ArrayList<Integer> positions = new ArrayList<>(word.length());
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == letter) {
+                positions.add(i);
+            }
+        }
+
+        return positions;
     }
 }
