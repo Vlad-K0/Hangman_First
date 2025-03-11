11e8319292f21113a20c428cb8e4abdf653588ef
Vlad Kovrigin <vladislavkov390@gmail.com>
2025-03-11 15:13:36 +0300

    feat: add game cycle

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,16 +1,36 @@
 package Hangman;
 
-import System.*;
+import Hangman.Enums.hangmanStages;
+import System.UserInterface;
+import System.Word;
+
+import java.util.ArrayList;
 
 public class Hangman {
-    private static final Display DISPLAY = new Display();
-    public static void StartGame(){
-        if(DISPLAY.UserStart()){
-            HangmanGame();
-        };
-    }
+    private static final UserInterface USER = new UserInterface();
+    private static int errorsCounter = 0;
+    private static int maxErrors = 6;
 
-    public static void HangmanGame(){
-        Word.GenerateWord();
+    public static void HangmanGame() {
+        errorsCounter = 0;
+        String secretWord = Word.getRandomWord();
+        StringBuilder mask = new StringBuilder(String.valueOf('_').repeat(secretWord.length()));
+        while (errorsCounter < maxErrors){
+            ArrayList<Integer> positions= Word.findLetterPositions(secretWord, USER.takeLetter());
+            if (positions.isEmpty()){
+                USER.notifyLetterNotFound();
+                errorsCounter++;
+            }
+            else {
+                USER.notifyLetterFound();
+                for (int i : positions){
+                    mask.setCharAt(i, secretWord.charAt(i));
+                }
+                System.out.println(mask);
+            }
+            if(!mask.toString().contains("_")){
+                break;
+            }
+        }
     }
 }
