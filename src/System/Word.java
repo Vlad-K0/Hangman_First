831ebe9967706b3d9acbd816b3a81583415dc1ed
Vlad Kovrigin <vladislavkov390@gmail.com>
2025-03-11 14:11:00 +0300

    feat: added a function that returns a random word from the dictionary

--- src/System/Word.java
+++ src/System/Word.java
@@ -1,4 +1,26 @@
 package System;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Random;
+
+
 public class Word {
+    public static final String PATH_DICTIONARY = "./resource/Dictionary11.txt";
+
+    public static String getRandomWord() {
+        try {
+            List<String> lines = Files.readAllLines(Path.of(PATH_DICTIONARY));
+
+            Random random = new Random();
+            return lines.get(random.nextInt(lines.size()));
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 }
