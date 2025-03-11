c459488bc8318f3e30d08cfb2b0cd284298a3e7f
Vlad Kovrigin <vladislavkov390@gmail.com>
2025-03-11 18:02:24 +0300

    feat: add print gallows stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -9,28 +9,44 @@ import java.util.ArrayList;
 public class Hangman {
     private static final UserInterface USER = new UserInterface();
     private static int errorsCounter = 0;
-    private static int maxErrors = 6;
+    private static final int maxErrors = 6;
 
     public static void HangmanGame() {
         errorsCounter = 0;
+        printStage();
         String secretWord = Word.getRandomWord();
         StringBuilder mask = new StringBuilder(String.valueOf('_').repeat(secretWord.length()));
-        while (errorsCounter < maxErrors){
-            ArrayList<Integer> positions= Word.findLetterPositions(secretWord, USER.takeLetter());
-            if (positions.isEmpty()){
+        while (errorsCounter < maxErrors) {
+            System.out.println("Слово: " + mask);
+            System.out.println("Ошибки " + "(" + errorsCounter + "): " + USER.getErrors());
+            char userInput = USER.takeLetter();
+            ArrayList<Integer> positions = Word.findLetterPositions(secretWord, userInput);
+            if (positions.isEmpty()) {
                 USER.notifyLetterNotFound();
+                USER.getErrors().add(userInput);
                 errorsCounter++;
-            }
-            else {
+            } else {
                 USER.notifyLetterFound();
-                for (int i : positions){
+                USER.getUserRight().add(userInput);
+                for (int i : positions) {
                     mask.setCharAt(i, secretWord.charAt(i));
                 }
                 System.out.println(mask);
             }
-            if(!mask.toString().contains("_")){
+            if (!mask.toString().contains("_")) {
+                USER.win();
                 break;
             }
+            printStage();
+        }
+        if (errorsCounter == maxErrors){
+            USER.lose(secretWord);
         }
     }
+
+    private static void printStage() {
+        hangmanStages[] stages = hangmanStages.values();
+        System.out.println(stages[errorsCounter].getState());
+    }
+
 }
